6771ebeccbe3d7d863f0d34049463fd715d729a5
zakaria <grarizakaria@gmail.com>
2024-04-22 20:54:53 +0100

    alx

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+    var task = 'I prefer const when I can.';
     return task;
-  }
+}
   
-  export function getLast() {
+export function getLast() {
     return ' is okay';
-  }
+}
   
-  export function taskNext() {
-    let combination = 'But sometimes let';
+export function taskNext() {
+    var combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
-  }
-  
+}
