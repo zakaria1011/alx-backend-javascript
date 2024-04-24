05e6397f1dabf500a12cd506b10f97b1e5da2b49
zakaria <grarizakaria@gmail.com>
2024-04-24 20:42:48 +0100

    alx

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,9 +4,9 @@ export default async function asyncUploadUser() {
   let result = {};
 
   try {
-     const photo =  await uploadPhoto();
-     const user = await createUser();
-     result = { photo, user };
+    const photo =  await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
   } catch (error) {
     result = { photo: null, user:null };
   }
