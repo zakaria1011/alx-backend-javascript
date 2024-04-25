d9cc4ab225df0981425efae7bd45f0e42bb66ee0
zakaria <grarizakaria@gmail.com>
2024-04-25 15:46:39 +0100

    alx

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+}
+export default HolbertonCourse;
