6530b14ef979b932f1460ed6c0bd41e0ad43c416
zakaria <grarizakaria@gmail.com>
2024-04-25 21:34:25 +0100

    alx

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,9 @@ class Building {
   constructor(sqft) {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
     }
     this.sqft = sqft;
