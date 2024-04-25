679ecbab544d8de773115d3ea4c0069a7a52217f
zakaria <grarizakaria@gmail.com>
2024-04-25 15:23:25 +0100

    alx

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
 import ClassRoom from "./0-classroom";
-export default function initializeRoom() {
+function initializeRooms() {
     return [
       new ClassRoom(19),
       new ClassRoom(20),
       new ClassRoom(34)
     ];
 }
+export default initializeRooms;
