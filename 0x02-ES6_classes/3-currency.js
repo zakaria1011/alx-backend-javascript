e97e0218de1885d43e24bbe92e1591602e39597c
zakaria <grarizakaria@gmail.com>
2024-04-25 16:08:48 +0100

    alx

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('code must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
