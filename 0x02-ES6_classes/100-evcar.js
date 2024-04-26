a419a2ae2fb6129e6435ea7863628f981bed5262
zakaria <grarizakaria@gmail.com>
2024-04-26 16:09:51 +0100

    alx

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
   cloneCar() {
