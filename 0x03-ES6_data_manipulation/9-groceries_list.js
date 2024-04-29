41c646ebc6042bcd2714453537b9f87e69fc180f
zakaria <grarizakaria@gmail.com>
2024-04-29 19:44:05 +0100

    map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+function groceriesList(){
+    const grocerie = new Map();
+    grocerie.set('Apples', 10);
+    grocerie.set('Tomatoes', 10);
+    grocerie.set('Pasta', 1);
+    grocerie.set('Rice', 1);
+    grocerie.set('Banana', 5);
+    return grocerie;
+    
+}
+export default groceriesList;
