823640312508bd57d1012370f5c3779a9e081e6e
zakaria <grarizakaria@gmail.com>
2024-04-30 18:43:49 +0100

    alx

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,23 @@
 interface Teacher {
     firstName: string;
     lastName: string;
-    readonly fullTimeEmployee : boolean;
+    readonly fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    [key: string]:any;
+    [key: string]: any;
 }
 
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const firstLetter = firstName.charAt(0).toUpperCase();
+    const fullName = `${firstLetter}. ${lastName}`;
+    return fullName;
+};
+
+
