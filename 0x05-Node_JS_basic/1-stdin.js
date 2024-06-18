7f41f395e285ada5de508260129b10328bdd9370
zakaria <grarizakaria@gmail.com>
2024-06-18 15:12:33 +0100

    task2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
-process.stdin.on('data', (data) => {
-  const input = data.trim();
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
   }
