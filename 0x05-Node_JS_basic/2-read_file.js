9171e5b1ada700bb711acb66efc2d04efe26663b
zakaria <grarizakaria@gmail.com>
2024-06-19 00:53:18 +0100

    task2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,26 @@
-const fs = require('fs')
-const path =  require('path')
+const fs = require('fs');
 
-function countStudents(filePath){
-    
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
+    const numberOfStudents = students.length;
+    console.log(`Number of students: ${numberOfStudents}`);
+    const csStudents = [];
+    const sweStudets = [];
+    students.forEach((student) => {
+      const [firstname, lastname, age, field] = student;
+      if (field === 'CS') {
+        csStudents.push(firstname);
+      } else if (field === 'SWE') {
+        sweStudets.push(firstname);
+      }
+    });
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudets.length}. List: ${sweStudets.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
+module.exports = countStudents;
