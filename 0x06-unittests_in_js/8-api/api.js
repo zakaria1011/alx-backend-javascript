3271adb7fcafb8f1dcb1b8f2c442f2063534c474
zakaria <grarizakaria@gmail.com>
2024-06-20 19:21:31 +0100

    task7

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+const server = app.listen(PORT, () => {
+    console.log(`API available on localhost port ${PORT}`);
+});
+module.exports = server
