ad4ccaf7288d44a28df9cc89690a185ffef46d44
zakaria <grarizakaria@gmail.com>
2024-06-20 19:44:26 +0100

    task8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,30 +1,26 @@
 const request = require('request');
-const { expect } = require('chai');
-const server = require('./api'); // Import the server instance
+const { expect } =  require('chai');
+const { response } = require('express');
 
 describe('index page', () => {
-  let serverInstance;
-
-  before((done) => {
-    serverInstance = require('./api');
-    setTimeout(done, 1000); // Corrected typo: setTimeout instead of settimeout
-  });
-
-  after(() => {
-    serverInstance.close(); // Close the server after tests complete
-  });
-
-  it('correct status code?', () => new Promise((done) => { // Ensure done is passed to the test function
-    request('http://localhost:7865', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done(); // Call done() to signal completion of the test
+    let server;
+    before((done) => {
+        server = require('./api');
+        setTimeout(done, 1000);
     });
-  }));
-
-  it('correct result', () => new Promise((done) => { // Ensure done is passed to the test function
-    request('http://localhost:7865', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done(); // Call done() to signal completion of the test
+    after(() => {
+        server.close()
     });
-  }));
+    it('Correct status code?', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('Correct result', (done) => {
+        request('http://localhost:7865', (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
 });
