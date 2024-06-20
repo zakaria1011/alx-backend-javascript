dc28530912f24f0d474b5b616ff2c12d22242534
zakaria <grarizakaria@gmail.com>
2024-06-20 19:23:38 +0100

    task7

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = 7865;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 const server = app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`);
+  console.log(`API available on localhost port ${PORT}`);
 });
-module.exports = server
+module.exports = server;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,26 @@
 const request = require('request');
-const { expect } =  require('chai');
+const { expect } = require('chai');
 const { response } = require('express');
 
 describe('index page', () => {
-    let server;
-    before((done) => {
-        server = require('./api');
-        setTimeout(done, 1000);
+  let server;
+  before((done) => {
+    server = require('./api');
+    setTimeout(done, 1000);
+  });
+  after(() => {
+    server.close();
+  });
+  it('correct status code?', () => new Promise((done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
-    after(() => {
-        server.close()
+  }));
+  it('correct result', () => new Promise((done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
     });
-    it('Correct status code?', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
-    });
-    it('Correct result', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
-            expect(body).to.equal('Welcome to the payment system');
-            done();
-        });
-    });
+  }));
 });
